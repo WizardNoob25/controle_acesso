99cde41b8c25f70239b444434eb125ed6d550b19
diogo vieira <diogo.vcastro25@gmail.com>
2025-06-02 12:13:44 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,11 +3,18 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // Essa é a configação em nível global, dei permitAll porque vou modifica em nivel de rota
 @Configuration
 public class SecurityConfig {
+
+    @Bean // Vai virar um componente que pode ser injetado em outros lugares
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
